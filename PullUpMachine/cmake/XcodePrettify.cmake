f76cac3b32bb5fbb4a6154ac26bc83163cce810e
dar-io-p <33568550+dar-io-p@users.noreply.github.com>
2023-11-30 13:19:37 +0000

    Update XcodePrettify.cmake

--- PullUpMachine/cmake/XcodePrettify.cmake
+++ PullUpMachine/cmake/XcodePrettify.cmake
@@ -1,5 +1,5 @@
 # No, we don't want our source buried in extra nested folders
-set_target_properties(SharedCode PROPERTIES FOLDER "")
+set_target_properties(${PROJECT_NAME} PROPERTIES FOLDER "")
 
 # The Xcode source tree should uhhh, still look like the source tree, yo
 source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR}/source PREFIX "" FILES ${SourceFiles})
