dcec2923710c0bcc022463edeea2d6f57641a090
Mr-SpaceMonkey <dario.potenza02@gmail.com>
2023-11-30 13:28:59 +0000

    removed blue pic

--- PullUpMachine/JuceLibraryCode/BinaryData.h
+++ PullUpMachine/JuceLibraryCode/BinaryData.h
@@ -8,14 +8,11 @@
 
 namespace BinaryData
 {
-    extern const char*   bluevinyl_png;
-    const int            bluevinyl_pngSize = 1497930;
-
     extern const char*   purplevinyl_png;
     const int            purplevinyl_pngSize = 259712;
 
     // Number of elements in the namedResourceList and originalFileNames arrays.
-    const int namedResourceListSize = 2;
+    const int namedResourceListSize = 1;
 
     // Points to the start of a list of resource names.
     extern const char* namedResourceList[];
